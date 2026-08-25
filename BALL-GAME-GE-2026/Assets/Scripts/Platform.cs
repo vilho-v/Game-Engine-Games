17c80a1b883efb35c213f8de50177c50acf29f1f
vilh <98084185+vilho-v@users.noreply.github.com>
2026-08-25 09:46:25 +0300

    jdhgidhio

--- BALL-GAME-GE-2026/Assets/Scripts/Platform.cs
+++ BALL-GAME-GE-2026/Assets/Scripts/Platform.cs
@@ -4,18 +4,29 @@ using UnityEngine;
 public class Platform : MonoBehaviour
 {
     Player player;
+
+    
+    bool active = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         player = FindFirstObjectByType<Player>();
+
+        // set layer to inactive on start , if plr already above it, gets set to active
+        gameObject.layer = LayerMask.NameToLayer("PlatformInactive");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(player.transform.position.y > transform.position.y)
+        // stop checking for plr position when already activated
+        if(active)
+            return;
+
+        if(player.transform.position.y > transform.position.y + transform.localScale.y)
         {
-            gameObject.layer = LayerMask.NameToLayer("PlatformInactive");
+            active = true;
+            gameObject.layer = LayerMask.NameToLayer("Platform");
         }
     }
 }
